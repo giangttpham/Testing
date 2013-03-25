f4ebe2011dff6d8bac969d181a0687f1536c4d58
Giang Pham <giangttpham@gmail.com>
2013-03-25 16:53:41 -0700

    Changed

--- Testing/src/Testing.java
+++ Testing/src/Testing.java
@@ -2,7 +2,7 @@
 public class Testing {
 	public static void main(String[] args){
 		System.out.println("hello");
-		System.out.println("its me");
+//		System.out.println("its me");
 		System.exit(0);
 	}
 }
